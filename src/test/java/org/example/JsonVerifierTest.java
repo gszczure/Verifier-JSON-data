f0bb3508b580ed9c24ad162aa84475ed9eb88115
gszczure <grzegorz.sz1202@outlook.com>
2024-04-16 21:58:01 +0200

    Add edge case

--- src/test/java/org/example/JsonVerifierTest.java
+++ src/test/java/org/example/JsonVerifierTest.java
@@ -7,34 +7,39 @@ import static org.junit.jupiter.api.Assertions.*;
 public class JsonVerifierTest {
 
     @Test
-    public void testVerifyMethod_ValidJson_ReturnsFalse() throws Exception {
+    public void testVerify_ValidJson_ReturnsFalse() throws Exception {
         String filePath = "/Users/grzegorz/IdeaProjects/Verifier/src/test/java/org/example/valid.json";
         boolean result = JsonVerifier.verifyMethod(filePath);
         assertFalse(result);
     }
 
     @Test
-    public void testVerifyMethod_InvalidResource_ReturnsTrue() throws Exception {
+    public void testVerify_InvalidResource_ReturnsTrue() throws Exception {
         String filePath = "/Users/grzegorz/IdeaProjects/Verifier/src/test/java/org/example/invalid_resource.json";
         boolean result = JsonVerifier.verifyMethod(filePath);
         assertTrue(result);
     }
 
     @Test
-    public void testVerifyMethod_MissingPolicyName_ThrowsException() {
+    public void testVerify_MissingPolicyName_ThrowsException() {
         String filePath = "/Users/grzegorz/IdeaProjects/Verifier/src/test/java/org/example/missing_json_object.json";
         assertThrows(Exception.class, () -> JsonVerifier.verifyMethod(filePath));
     }
 
     @Test
-    public void testVerifyMethod_InvalidPolicyName_ThrowsException() {
+    public void testVerify_InvalidPolicyName_ThrowsException() {
         String filePath = "/Users/grzegorz/IdeaProjects/Verifier/src/test/java/org/example/invalid_policy_name.json";
         assertThrows(Exception.class, () -> JsonVerifier.verifyMethod(filePath));
     }
 
     @Test
-    public void testVerifyMethod_InvalidEffect_ThrowsException() {
+    public void testVerify_InvalidEffect_ThrowsException() {
         String filePath = "/Users/grzegorz/IdeaProjects/Verifier/src/test/java/org/example/invalid_effect.json";
+        assertThrows(Exception.class, () -> JsonVerifier.verifyMethod(filePath));
+    }
+    @Test
+    public void testVerify_LongPolicyName_ThrowsException() {
+        String filePath = "/Users/grzegorz/IdeaProjects/Verifier/src/test/java/org/example/long_policy_name.json";
         assertThrows(Exception.class, () -> JsonVerifier.verifyMethod(filePath));
     }
 }
